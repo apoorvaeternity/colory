077836ccca892c4f09a3a1b4f3a9b605efb55c06
Apoorva Pandey <apoorvapandey365@gmail.com>
2017-10-15 17:07:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Colory
+> Fetches color name using hex value.
+
+
+## Usage example
+
+
+```python
+
+>>> from colors import Color
+>>> a=Color('#FFFFFF')
+>>> a.nearest_match()
+'Exact Match Found: White'
+>>> b=Color('#FFFF10')	
+>>> b.nearest_match()
+'Nearest Match: Yellow'
+
+```
+
+
+
+## Meta
+
+Apoorva Pandey – apoorvapandey365@gmail.com
+
+Distributed under the BSD license.
+
+[https://github.com/apoorvaeternity](https://github.com/apoorvaeternity)
+
+## Contributing
+
+1. Fork it (<https://github.com/apoorvaeternity/colory/>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -m 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
---
+++ colors.py
@@ -0,0 +1,39 @@
+"""
+This script tries to find the name of color using a given hex value
+List of colors has been obtained from
+https://en.wikipedia.org/wiki/List_of_colors:_A-F
+https://en.wikipedia.org/wiki/List_of_colors:_G-M
+https://en.wikipedia.org/wiki/List_of_colors:_N-Z
+
+"""
+
+colors = dict()
+with open('color_list.csv') as f:
+    lis = [line.split(',') for line in f]
+    for hex_val, name in lis:
+        colors[hex_val] = (name.rstrip(), int(hex_val, base=16))
+
+
+class Color:
+    def __init__(self, value: "hexadecimal value of color"):
+        self.value = value
+        if '#' in value:
+            self.value = value.replace('#', '0x')
+
+    def nearest_match(self):
+        # Find the nearest matching color from the list
+        hex_val = hex(int(self.value, base=16))
+
+        if hex_val in list(colors.keys()):
+            return "Exact Match Found: " + colors[hex_val][0]
+        else:
+            # This is not the correct way of comparing colors
+            # See https://en.wikipedia.org/wiki/Color_difference
+            color = None
+            min = 16777215  # FFFFFF
+            color_int = int(self.value, base=16)
+            for item in colors.values():
+                if abs(item[1] - color_int) < min:
+                    min = abs(item[1] - color_int)
+                    color = item[0]
+            return "Nearest Match: " + color
