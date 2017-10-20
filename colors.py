4f7d0aaa5039a9980e98ac405950b38cc085ea31
Teg khanna <tegkhanna@gmail.com>
2017-10-20 08:07:26 +0530

    Feature/rgb_cartesian (#6)
    * added cartesian difference. much much more accurate

--- colors.py
+++ colors.py
@@ -42,10 +42,31 @@ class Color:
             # This is not the correct way of comparing colors
             # See https://en.wikipedia.org/wiki/Color_difference
             color = None
-            min = 16777215  # FFFFFF
-            color_int = int(self.value, base=16)
+            min_diff = 16777215  # FFFFFF
             for item in colors[self.colset].values():
-                if abs(item[1] - color_int) < min:
-                    min = abs(item[1] - color_int)
+                color_to_compare_hex = "0x{:06x}".format(item[1])
+                if self.euclean_calculate(self.value, color_to_compare_hex) < min_diff:
+                    min_diff = self.euclean_calculate(self.value, color_to_compare_hex)
                     color = item[0]
             return "Nearest Match: " + color
+            
+    def euclean_calculate(self, c1, c2):
+        # self un-needed here but python will riase argument exception since it passes it automatically
+        # check the last formula in https://en.wikipedia.org/wiki/Color_difference#Euclidean
+        #c1 rgb
+        c1 = c1.replace('0x', '')
+        c2 = c2.replace('0x', '')
+        c1_r_int = int(c1[0:2], base=16)
+        c1_g_int = int(c1[2:4], base=16)
+        c1_b_int = int(c1[4:6], base=16)
+        #c2 rgb
+        c2_r_int = int(c2[0:2], base=16)
+        c2_g_int = int(c2[2:4], base=16)
+        c2_b_int = int(c2[4:6], base=16)
+        #delta square
+        delta_r = (c1_r_int - c2_r_int)**2 
+        delta_g = (c1_g_int - c2_g_int)**2 
+        delta_b = (c1_b_int - c2_b_int)**2
+        r = (c1_r_int + c2_r_int)/2
+        delta_c = ((2 + r/256)*delta_r + 4*delta_g + (2 + (255 - r)/2)*delta_b)
+        return delta_c**0.5
